75cf362d2c55d08243da55f9dbe991e4261bd1d3
Maurizio Drocco <maurizio.drocco@ibm.com>
2020-03-26 17:34:12 -0400

    simplified rebuild.sh

--- xnba/rebuild.sh
+++ xnba/rebuild.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-tar xvfj xnba-1.0.3.tar.bz2 
 cd xnba-1.0.3
 cp -rL ../debian .
 dpkg-buildpackage -uc -us  -aamd64 
-cd -
-rm -rf xnba-1.0.3 
-cp `pwd`/debian/`dh_listpackages`/tftpboot/xcat/xnba.kpxe `pwd`/binary/
-cp `pwd`/debian/`dh_listpackages`/tftpboot/xcat/xnba.efi  `pwd`/binary/
+
+echo "> efi binary:"
+ls -l debian/xnba-undi/tftpboot/xcat/xnba.efi
